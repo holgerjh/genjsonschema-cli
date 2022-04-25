f0f8f8ac8dd309c431a7718662fef3670a801605
Holger Heinz <79228197+holgerjh@users.noreply.github.com>
2022-04-25 21:37:29 +0200

    call command from main.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
@@ -13,22 +11,10 @@ func RootCmd() *cobra.Command {
 		Long: `This application is used to generate JSON Schemas from YAML or JSON files.
 For more information, see genjsonschema-cli create --help 
 `,
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
-		// Run: func(cmd *cobra.Command, args []string) { },
 	}
 	command.AddCommand(
 		generateCreateCommand(),
 	)
 	return command
 
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := RootCmd().Execute()
-	if err != nil {
-		os.Exit(1)
-	}
 }
--- main.go
+++ main.go
@@ -4,8 +4,15 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/holgerjh/genjsonschema-cli/cmd"
+import (
+	"os"
+
+	"github.com/holgerjh/genjsonschema-cli/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	err := cmd.RootCmd().Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
